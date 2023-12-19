43b55b742b7919299dab2d65b2ad2888336965c7
jh.jeong <jh.jeong@okestro.com>
2023-12-19 10:01:16 +0900

    2023.12.19 | index.js, main.js 정렬

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 
 
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -8,24 +8,24 @@ Vue.use(VueRouter);
 
 // 라우트 정의
 const routes = [
-  {
-    path: '/',
-    name: 'list',
-    component: ListPage,
-  },
-  {
-    path: '/detail/:id',
-    name: 'detail',
-    component: DetailPage,
-    props: true, // 라우트 매개변수를 컴포넌트의 props로 전달
-  }
+    {
+        path: '/',
+        name: 'list',
+        component: ListPage,
+    },
+    {
+        path: '/detail/:id',
+        name: 'detail',
+        component: DetailPage,
+        props: true, // 라우트 매개변수를 컴포넌트의 props로 전달
+    }
 ];
 
 // Vue Router 인스턴스 생성
 const router = new VueRouter({
-  mode: 'history', // HTML5 History 모드 사용
-  base: process.env.BASE_URL, // 라우터의 기본 경로
-  routes, // 라우트 목록
+    mode: 'history', // HTML5 History 모드 사용
+    base: process.env.BASE_URL, // 라우터의 기본 경로
+    routes, // 라우트 목록
 });
 
 // 라우터 인스턴스 내보내기
